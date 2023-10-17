5b57bc1e8e2e36b43a0becf1b0b4ce42a480b63f
mstewart5 <104705110+mstewart5@users.noreply.github.com>
2023-10-17 01:09:11 -0500

    adding react project

---
+++ le-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="bruh">
+      <div className="content">
+        <h1>hello</h1>
+      </div>
+    </section>
+  );
+}
+export default App;
